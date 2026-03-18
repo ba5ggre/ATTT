9aae8381583ea05e1850c1da7be86fb25a86b3c2
Duong Van Bao <baobinh2005a@gmail.com>
2026-03-18 19:47:37 +0700

    nhap key

--- ATTT/RC4.cpp
+++ ATTT/RC4.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
-#include <string.h>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+void swap_int(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int N;
+    cout << "Nhap N : ";
+    cin >> N;
+    cin.ignore();
+
+    vector<int> S(N);
+
+    // Chọn kiểu key
+    cout << "Chon kieu key:\n1. Ky tu (ASCII)\n2. So (danh sach)\nLua chon: ";
+    int choice;
+    cin >> choice;
+    cin.ignore();
+
+    vector<int> K;
+
+    
