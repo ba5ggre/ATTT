b731894ec042012b1acf44053cc0c31c6d666327
Duong Van Bao <baobinh2005a@gmail.com>
2026-03-18 19:49:15 +0700

    Key nhap chi tiet

--- ATTT/RC4.cpp
+++ ATTT/RC4.cpp
@@ -25,4 +25,28 @@ int main() {
 
     vector<int> K;
 
-    
+    if (choice == 1) {
+        // Key dạng chuỗi
+        char key[256];
+        cout << "Nhap key: ";
+        cin.getline(key, 256);
+
+        int len = strlen(key);
+        for (int i = 0; i < len; i++) {
+            K.push_back((unsigned char)key[i]);
+        }
+    } else {
+        // Key dạng số (vd: 2 4 1 7)
+        cout << "Nhap so phan tu key: ";
+        int len;
+        cin >> len;
+
+        cout << "Nhap key (cach nhau boi space): ";
+        for (int i = 0; i < len; i++) {
+            int x;
+            cin >> x;
+            K.push_back(x);
+        }
+    }
+
+    cin.ignore();
